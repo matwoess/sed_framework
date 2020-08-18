ea6fcb3207e12a0816ecbf15936f12f8b02249ae
TheInsayn <mat.woess@gmail.com>
2020-08-18 22:11:16 +0200

    extract features in dataset with librosa

--- datasets.py
+++ datasets.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
-
+import csv
 import glob
 import os
 
+import librosa
 import numpy as np
 from torch.utils.data import Dataset
 
@@ -21,17 +22,50 @@ class BaseDataset(Dataset):
         return self.audio_files[idx], idx
 
 
+class SpectrogramDataset(Dataset):
+    def __init__(self, dataset: Dataset, data_path: str = os.path.join('data', 'dev')):
+        self.dataset = dataset
+        if not os.path.exists(data_path):
+            raise ValueError(f'dataset path "{data_path}" does not exist')
+
+    def __len__(self):
+        return self.dataset.__len__()
+
+    def __getitem__(self, idx):
+        audio_file, _ = self.dataset.__getitem__(idx)
+        audio, sr = librosa.load(audio_file)
+        spec = np.abs(librosa.stft(audio, n_fft=2048))
+        mels = librosa.mel_frequencies(n_mels=64)
+        mfccs = librosa.feature.mfcc(audio, sr=sr)
+        return spec, mfccs, mels, sr, idx
+
+
+def read_annotations(annotation_files):
+    annotations = []
+    for annotation_file in annotation_files:
+        with open(annotation_file, 'r') as af:
+            content = list(csv.reader(af, delimiter='\t'))
+        data = {
+            'onsets': [on for on, _, _ in content],
+            'offsets': [off for _, off, _ in content],
+            'events': [evt for _, _, evt in content]
+        }
+        annotations.append(data)
+    return annotations
+
+
 class TrainingDataset(Dataset):
     def __init__(self, dataset: Dataset, data_path: str = os.path.join('data', 'dev')):
         self.dataset = dataset
         if not os.path.exists(data_path):
             raise ValueError(f'dataset path "{data_path}" does not exist')
         meta_path = os.path.join(data_path, 'meta')
-        self.annotations = sorted(glob.glob(os.path.join(meta_path, '**/*.ann'), recursive=True))
+        annotation_files = sorted(glob.glob(os.path.join(meta_path, '**/*.ann'), recursive=True))
+        self.annotations = read_annotations(annotation_files)
 
     def __len__(self):
-        return len(self.annotations)
+        return self.dataset.__len__()
 
     def __getitem__(self, idx):
-        audio_file, _ = self.dataset.__getitem__(idx)
-        return audio_file, self.annotations[idx], idx
+        spec, mfccs, mels, sr, _ = self.dataset.__getitem__(idx)
+        return spec, mfccs, mels, sr, self.annotations[idx], idx
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import torch
 import numpy as np
 from torch.utils.tensorboard import SummaryWriter
 
-from datasets import BaseDataset, TrainingDataset
+from datasets import BaseDataset, TrainingDataset, SpectrogramDataset
 
 
 def main(results_path: str, network_config: dict, eval_settings: dict, classes: list, learning_rate: int = 1e-3,
@@ -15,7 +15,13 @@ def main(results_path: str, network_config: dict, eval_settings: dict, classes: 
     os.makedirs(plots_path, exist_ok=True)
     writer = SummaryWriter(log_dir=os.path.join(results_path, 'tensorboard'))
     base_dataset = BaseDataset()
-    training_dataset = TrainingDataset(dataset=base_dataset)
+    spectrogram_dataset = SpectrogramDataset(dataset=base_dataset)
+    training_dataset = TrainingDataset(dataset=spectrogram_dataset)
+    base_dataset_eval = BaseDataset(data_path=os.path.join('data', 'eval'))
+    spectrogram_dataset_eval = SpectrogramDataset(dataset=base_dataset_eval)
+    training_dataset_eval = TrainingDataset(dataset=spectrogram_dataset_eval)
+    print(training_dataset[0])
+    print(training_dataset_eval[0])
 
 
 if __name__ == '__main__':
