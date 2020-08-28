00dbb69b4686049e55be62f03db64a3ca114aafe
TheInsayn <mat.woess@gmail.com>
2020-08-28 15:32:55 +0200

    implemented squeeze layer istead of `(nfeatures/4, 1)` kernel layer

--- architectures.py
+++ architectures.py
@@ -20,39 +20,45 @@ class SimpleCNN(torch.nn.Module):
 
         layers = []
         in_channels = n_kernels
-        n_kernels *= 2
-        layers.append(Conv2d(in_channels=in_channels, out_channels=n_kernels, kernel_size=3, stride=1, padding=1))
+        layers.append(Conv2d(in_channels=in_channels, out_channels=2 * n_kernels, kernel_size=3, stride=1, padding=1))
         layers.append(ReLU())
-        layers.append(BatchNorm2d(n_kernels))
+        layers.append(BatchNorm2d(2 * n_kernels))
         layers.append(MaxPool2d((2, 1)))
         self.layers2 = torch.nn.Sequential(*layers)
 
         layers = []
-        in_channels = n_kernels
-        layers.append(Conv2d(in_channels=in_channels, out_channels=n_kernels, kernel_size=(n_features // 4, 1),
-                             stride=1, padding=0))
+        in_channels = 2 * n_kernels
+        layers.append(Conv2d(in_channels=in_channels, out_channels=2 * n_kernels, kernel_size=3, stride=1, padding=1))
         layers.append(ReLU())
-        layers.append(BatchNorm2d(n_kernels))
+        layers.append(BatchNorm2d(2 * n_kernels))
+        layers.append(MaxPool2d((2, 1)))
         # layers.append(torch.nn.AvgPool2d(n_kernels))
         self.layers3 = torch.nn.Sequential(*layers)
 
+        self.squeeze = Squeeze()
+
         for i in range(out_features):
-            layers = [Linear(n_kernels, 1), Sigmoid()]
+            layers = [Linear(2 * n_kernels * n_features // 8, 1), Sigmoid()]
             self.__setattr__(f'output_layer{i}', Sequential(*layers))
 
     def forward(self, x):
         x = self.layers1(x)  # (B, 1, F, T) -> (B, K, F/2, T)
         x = self.layers2(x)  # (B, K, F/2, T) -> (B, 2K, F/4, T)
-        x = self.layers3(x)  # (B, 2K, F/4, T) -> (B, 2K, 1, T)
-        x.squeeze_(2)  # (B, 2K, 1, T) -> (B, 2K, T)
-        fnn_input = x.transpose(1, 2)  # (B, 2K, T) -> (B, T, 2K)
+        x = self.layers3(x)  # (B, 2K, F/4, T) -> (B, 2K, F/8, T)
+        x = self.squeeze(x)  # (B, 2K, F/8, T) -> (B, 2K*F/8, T)
+        fnn_input = x.transpose(1, 2)  # (B, 2K*F/8, T) -> (B, T, 2K*F/8)
         predictions = []
         for idx in range(self.out_features):
             fnn = self.__getattr__(f'output_layer{idx}')
-            class_predictions = fnn(fnn_input)  # (B, T, 2K) -> (B, T, 1)
+            class_predictions = fnn(fnn_input)  # (B, T, 2K*F/8) -> (B, T, 1)
             class_predictions = class_predictions.transpose(2, 1)  # (B, T, 1) -> (B, 1, T)
             predictions.append(class_predictions)  # predictions += (B, 1, T)
         return torch.stack(predictions, dim=2).squeeze(1)  # [predictions] -> (B, 1, n_out, T) -> (B, n_out, T)
+
+
+class Squeeze(torch.nn.Module):
+    def forward(self, x):
+        return x.view(x.shape[0], -1, x.shape[-1])
 
 
 class Flatten(torch.nn.Module):
