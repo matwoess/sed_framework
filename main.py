metric computation function; fixes for both-scenes-mode; config updates

main.py
@@ -37,7 +37,9 @@ def main(results_path: str, network_config: dict, eval_settings: dict, classes: 
          ):
     """Main function that takes hyperparameters, creates the architecture, trains the model and evaluates it"""
     plots_path = os.path.join(results_path, 'plots')
+    metrics_path = os.path.join(results_path, 'metrics')
     os.makedirs(plots_path, exist_ok=True)
+    os.makedirs(metrics_path, exist_ok=True)
     writer = SummaryWriter(log_dir=os.path.join(results_path, 'tensorboard'))
     training_dataset = BaseDataset(scenes=scenes, features=feature_type)
 
@@ -51,6 +53,7 @@ def main(results_path: str, network_config: dict, eval_settings: dict, classes: 
     optimizer = torch.optim.Adam(net.parameters(), lr=learning_rate, weight_decay=weight_decay)
 
     plot_at = eval_settings['plot_at']
+    metrics_at = eval_settings['metrics_at']
     best_validation_loss = np.inf  # best validation loss so far
     progress_bar = tqdm.tqdm(total=n_updates, desc=f"loss: {np.nan:7.5f}", position=0)
 
@@ -67,8 +70,8 @@ def main(results_path: str, network_config: dict, eval_settings: dict, classes: 
 
     while update <= n_updates:
         fold_idx = (fold_idx + 1) % 4
-        train_set = Subset(training_dataset, training_dataset.get_fold_indices(scenes[0], fold_idx)[0])
-        val_set = Subset(training_dataset, training_dataset.get_fold_indices(scenes[0], fold_idx)[1])
+        train_set = Subset(training_dataset, training_dataset.get_fold_indices(scenes, fold_idx)[0])
+        val_set = Subset(training_dataset, training_dataset.get_fold_indices(scenes, fold_idx)[1])
         train_set = FeatureDataset(train_set, classes, excerpt_size=excerpt_size)
         val_set = FeatureDataset(val_set, classes, excerpt_size=excerpt_size)
         train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=False, num_workers=0)
@@ -87,11 +90,16 @@ def main(results_path: str, network_config: dict, eval_settings: dict, classes: 
             loss.backward()
             optimizer.step()
 
-            # Plot output
+            # plot output
             if update % plot_at == 0 and update > 0:
                 plot_targets = targets.detach().numpy()
                 plot_predictions = predictions.detach().numpy()
-                utils.plot(plot_targets, plot_predictions, classes, excerpt_size, plots_path, update)
+                utils.plot(plot_targets, plot_predictions, classes, plots_path, update)
+            # compute metrics
+            if update % metrics_at == 0 and update > 0:
+                metric_targets = targets.detach().numpy()
+                metric_predictions = predictions.detach().numpy()
+                utils.compute_metrics(metric_targets, metric_predictions, metrics_path, update)
 
             # update progress and update-counter
             progress_bar.set_description(f"loss: {loss:7.5f}", refresh=True)
@@ -126,11 +134,11 @@ def main(results_path: str, network_config: dict, eval_settings: dict, classes: 
     net = torch.load(os.path.join(results_path, 'best_model.pt'))
     fold_idx = 0
 
-    train_set = Subset(training_dataset, training_dataset.get_fold_indices(scenes[0], fold_idx)[0])
+    train_set = Subset(training_dataset, training_dataset.get_fold_indices(scenes, fold_idx)[0])
     train_set = FeatureDataset(train_set, classes, excerpt_size=excerpt_size)
     train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=False, num_workers=0)
 
-    val_set = Subset(training_dataset, training_dataset.get_fold_indices(scenes[0], fold_idx)[1])
+    val_set = Subset(training_dataset, training_dataset.get_fold_indices(scenes, fold_idx)[1])
     val_set = FeatureDataset(val_set, classes, excerpt_size=excerpt_size)
     val_loader = DataLoader(val_set, batch_size=batch_size, shuffle=False, num_workers=0)
 
@@ -148,7 +156,7 @@ def main(results_path: str, network_config: dict, eval_settings: dict, classes: 
     print(f"training loss: {train_loss}")
 
     # Write result to separate file
-    with open(os.path.join(results_path, 'results.txt'), 'w') as f:
+    with open(os.path.join(results_path, 'losses.txt'), 'w') as f:
         print(f"Scores:", file=f)
         print(f"test loss: {test_loss}", file=f)
         print(f"validation loss: {val_loss}", file=f)

utils.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import os
 import zipfile
+from typing import Dict
 
 import numpy as np
 import requests
@@ -8,7 +9,8 @@ import tqdm
 from matplotlib import pyplot as plt
 
 
-def plot(targets: np.ndarray, predictions: np.ndarray, classes: list, length: int, path: str, iteration: int) -> None:
+def plot(targets: np.ndarray, predictions: np.ndarray, classes: list, path: str, iteration: int) -> None:
+    print('plotting results...')
     os.makedirs(path, exist_ok=True)
     cmap = ['b', 'r', 'g', 'y', 'k', 'c', 'm', 'b', 'r', 'g', 'y', 'k', 'c', 'm']
     label_size = 6 if len(classes) < 12 else 4
@@ -20,6 +22,7 @@ def plot(targets: np.ndarray, predictions: np.ndarray, classes: list, length: in
     to_plot = [targets, thresh_predictions, errors]
     plot_titles = ['target', 'prediction', 'error']
     batches = targets.shape[0]
+    length = targets.shape[-1]
     for b in range(batches):
         plt.rc('ytick', labelsize=label_size)
         fig, ax = plt.subplots(3, 1)
@@ -38,6 +41,46 @@ def plot(targets: np.ndarray, predictions: np.ndarray, classes: list, length: in
         fig.savefig(os.path.join(path, f"{iteration:07d}_{b:02d}.png"), dpi=500)
         fig.clf()
     del fig
+
+
+def compute_metrics(targets: np.ndarray, predictions: np.ndarray, save_path: str, iteration: int) -> None:
+    print('computing metrics...')
+    os.makedirs(save_path, exist_ok=True)
+    targets = targets == 1
+    targets = targets.reshape(-1)
+    n_samples = len(targets)
+    threshold = 0.5
+    predictions = predictions.reshape(-1)
+    predictions = [b for b in predictions >= threshold]
+    if not np.any(targets):
+        print('no positive targets, aborting...')
+        return
+    if not np.any(predictions):
+        print('no positive predictions, aborting...')
+        return
+    tp = sum([1 for p, t in zip(predictions, targets) if t and p])
+    tn = sum([1 for p, t in zip(predictions, targets) if not t and not p])
+    fp = sum([1 for p, t in zip(predictions, targets) if not t and p])
+    fn = sum([1 for p, t in zip(predictions, targets) if t and not p])
+    assert tp + tn + fp + fn == n_samples
+    P = tp + fn
+    N = tn + fp
+    tpr = 1.0 if P == 0 else tp / P
+    tnr = 1.0 if N == 0 else tn / N
+    fpr = 1 - tnr
+    fnr = 1 - tpr
+    ppr = tp / (tp + fp)
+    acc = (tp + tn) / n_samples
+    bacc = (tpr + tnr) / 2
+    f1score = 0.0 if tpr + ppr == 0 else 2 * (tpr * ppr) / (tpr + ppr)
+    metrics = {'F1-score': f1score, 'error rate / false negative rate': fnr,
+               'accuracy': acc, 'balanced accuracy': bacc,
+               'recall / true positive rate': tpr, 'precision / positive predictive rate': ppr,
+               'fall-out / false positive rate': fpr, 'true negative rate': tnr}
+    with open(os.path.join(save_path, f'{iteration:07d}.txt'), 'w') as f:
+        print(f"Metrics:", file=f)
+        for key in metrics.keys():
+            print(f'{key}: {metrics[key]}', file=f)
 
 
 def download_url(url, save_path, description, chunk_size=4096):
@@ -95,4 +138,5 @@ if __name__ == '__main__':
     test_classes = ["bird singing", "children shouting", "wind blowing"]
     test_path = 'results/plots'
     test_update = 1
-    plot(test_targets, test_predictions, test_classes, test_excerpt_length, test_path, test_update)
+    # plot(test_targets, test_predictions, test_classes, test_path, test_update)
+    compute_metrics(test_targets, test_predictions, 'results/metrics', test_update)